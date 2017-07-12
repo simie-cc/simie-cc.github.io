f01d9ff22d77d57997d892b3ee7fd65d356f194c
SimieCC <cscxcs@gmail.com>
2017-07-12 08:41:09 +0800

    :construction_worker: makerelease script add version support

--- makerelease.sh
+++ makerelease.sh
@@ -2,9 +2,36 @@
 #
 #  make release script
 #
+if [ ! -z "$(git status --porcelain)" ]; then
+    echo "Working directory not clean."
+    exit 2
+fi
+
+echo "Current version: "
+echo "=========="
+cat src/environments/version.ts
+echo
+echo "=========="
+
+echo
+echo "Enter new version:"
+read ver
+
+if [ ! "$ver" ]; then
+    echo "Not valid version"
+    exit 1
+fi
+
+cat << EOF > src/environments/version.ts
+export const Version = '${ver}';
+export const LastModified = '`date +%Y%m%d`';
+EOF
+
+git add src/environments/version.ts
+git commit -m ":bookmark: Bump version number"
+
 set -o verbose
 
-git status
 ng build --prod
 git checkout master
 git pull
